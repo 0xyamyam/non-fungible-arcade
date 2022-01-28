fad8a09b519d1fc4720dcdbd3b759c66f9d46511
0xyamyam <0xyamyam@gmail.com>
2022-01-28 20:49:35 +0000

    feature(homepage): Add backgrounds and hero section

--- src/Homepage.vue
+++ src/Homepage.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>home</div>
+  <div class="landscape-bg mt-0 md:-mt-32">
+    <div class="container mx-auto flex flex-col md:flex-row md:justify-center md:items-center md:pt-60">
+      <div class="text-center md:text-left">
+        <h1 class="title text-4xl md:text-7xl uppercase mt-4 mb-4">Reinventing the<br/>arcade experience</h1>
+        <h2 class="title text-xl md:text-4xl uppercase text-[#DB38B9] mb-4">For the metaverse</h2>
+        <h3 class="text-sm md:text-auto mb-8">The 80's are back for Web3.</h3>
+        <a href="#" title="" class="inline-block mb-8 title uppercase text-xl leading-none text-[#DB38B9] border-2 border-[#DB38B9] rounded-full pt-3 pb-2 px-8">Play Now</a>
+      </div>
+      <img src="./assets/arcade-system.png" class="w-72 md:w-auto mx-auto md:mx-0" />
+    </div>
+  </div>
+  <div class="grid-bg py-96">home</div>
+  <div class="gradient-bg py-96">home</div>
 </template>
 
 <script lang="ts">
