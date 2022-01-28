08df28bf50b518ce618220ad29c4938143de98f4
0xyamyam <0xyamyam@gmail.com>
2022-01-28 17:42:30 +0000

    chore(setup): Install router and boilerplate routes

---
+++ src/NotFound.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>not found</div>
+</template>
+
+<script lang="ts">
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,18 @@
 import { createApp } from 'vue'
 import './styles.css'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
+import Homepage from './Homepage.vue'
+import NotFound from './NotFound.vue'
 
-createApp(App).mount('#app')
+const router = createRouter({
+    history: createWebHistory('/'),
+    routes: [
+        { path: '/', name: 'home', component: Homepage },
+        { path: '/:pathMatch(.*)*', name: '404', component: NotFound }
+    ]
+})
+
+createApp(App)
+    .use(router)
+    .mount('#app')
